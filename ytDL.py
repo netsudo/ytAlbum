6b45c8c8a74cc147af1c0de68a853ddc2377d9e3
netsudo <netsudo@users.noreply.github.com>
2016-09-12 23:56:06 -0700

    Takes youtube link, purges useless lines, deletes numbered bullets, splits timestamp and song name but keepts them organized. Need to split video with ffmpeg next, then integrate into Django.

---
+++ ytDL.py
@@ -0,0 +1,252 @@
+import pafy
+import os
+import sys
+import re
+from collections import namedtuple
+
+class downloadProcess:
+    def __init__(self):
+        self.artistName = ''
+        self.albumName = ''
+        self.downloadLink = ''
+
+    def downloadMp3(self):
+        video = pafy.new(self.downloadLink)
+        audiostream = video.getbestaudio()
+
+        audiostream.download()
+
+    def grabDescrip(self):
+        video = pafy.new(self.downloadLink)
+        vidDescription = video.description
+
+        while True:
+            try:
+                if vidDescription != None:
+                    return vidDescription.encode('utf-8').splitlines()
+                else:
+                    break
+
+            except:
+                continue
+
+def uselessLines():
+    good_list = []
+    for line in downloadiT.grabDescrip():
+        if re.search(r'^.*\d+:\d\d', line):
+            good_list.append(line)
+
+    return good_list
+
+def numberedCheck():
+    good_list = []
+    line = uselessLines()[0]
+    if line.startswith('1.') and not line[0].startswith('1. '):
+        return 0
+    elif line.startswith('1. '):
+        return 1
+    elif line.startswith('(1)') and not line.startswith('(1) '):
+        return 2
+    elif line.startswith('(1) '):
+        return 3
+    elif line.startswith('1-') and not line.startswith('1- '):
+        return 4
+    elif line.startswith('1 - '):
+        return 5
+    elif line.startswith('1 -') and not line.startswith('1 - '):
+        return 6
+    elif line.startswith('1- '):
+        return 7
+    elif line.startswith('01.') and not line.startswith('01. '):
+        return 8
+    elif line.startswith('01. '):
+        return 9
+    elif line.startswith('01 - '):
+        return 10
+    elif line.startswith('01- '):
+        return 11
+    elif line.startswith('01-') and not line.startswith('01- '):
+        return 12
+    elif line.startswith('01 -') and not line.startswith('01 - '):
+        return 13
+    else:
+        return False
+
+def numberReplace():
+    i = 0
+    unnumberedList = []
+    bulletType = numberedCheck()
+    if bulletType == 0:
+        for line in uselessLines():
+            unnumberedLine = line.replace(str(i + 1) + '.', '')
+            i+=1
+
+            unnumberedList.append(unnumberedLine)
+        return unnumberedList
+
+    elif bulletType == 1:
+        for line in uselessLines():
+            unnumberedLine = line.replace(str(i + 1) + '. ', '')
+            i+=1
+
+            unnumberedList.append(unnumberedLine)
+        return unnumberedList
+
+    elif bulletType == 2:
+        for line in uselessLines():
+            unnumberedLine = line.replace('(' + str(i + 1) + ')', '')
+            i+=1
+
+            unnumberedList.append(unnumberedLine)
+        return unnumberedList
+
+    elif bulletType == 3:
+        for line in uselessLines():
+            unnumberedLine = line.replace('(' + str(i + 1) + ') ', '')
+            i+=1
+
+            unnumberedList.append(unnumberedLine)
+        return unnumberedList
+
+    elif bulletType == 4:
+        for line in uselessLines():
+            unnumberedLine = line.replace(str(i + 1) + '-', '')
+            i+=1
+
+            unnumberedList.append(unnumberedLine)
+        return unnumberedList
+
+    elif bulletType == 5:
+        for line in uselessLines():
+            unnumberedLine = line.replace(str(i + 1) + ' - ', '')
+            i+=1
+
+            unnumberedList.append(unnumberedLine)
+        return unnumberedList
+
+    elif bulletType == 6:
+        for line in uselessLines():
+            unnumberedLine = line.replace(str(i + 1) + ' -', '')
+            i+=1
+
+            unnumberedList.append(unnumberedLine)
+        return unnumberedList
+
+    elif bulletType == 7:
+        for line in uselessLines():
+            unnumberedLine = line.replace(str(i + 1) + '- ', '')
+            i+=1
+
+            unnumberedList.append(unnumberedLine)
+        return unnumberedList
+
+    elif bulletType == 8:
+        for line in uselessLines():
+            if i < 10:
+                unnumberedLine = line.replace('0' + str(i + 1) + '.', '')
+                i+=1
+
+                unnumberedList.append(unnumberedLine)
+            else:
+                unnumberedLine = line.replace(str(i+1) + '.')
+
+                unnumberedList.append(unnumberedLine)
+
+        return unnumberedList
+
+    elif bulletType == 9:
+        for line in uselessLines():
+            if i < 10:
+                unnumberedLine = line.replace('0' + str(i + 1) + '. ', '')
+                i+=1
+
+                unnumberedList.append(unnumberedLine)
+            else:
+                unnumberedLine = line.replace(str(i+1) + '. ')
+
+                unnumberedList.append(unnumberedLine)
+
+        return unnumberedList
+
+    elif bulletType == 10:
+        for line in uselessLines():
+            if i < 10:
+                unnumberedLine = line.replace('0' + str(i + 1) + ' - ', '')
+                i+=1
+
+                unnumberedList.append(unnumberedLine)
+            else:
+                unnumberedLine = line.replace(str(i+1) + ' - ')
+
+                unnumberedList.append(unnumberedLine)
+
+        return unnumberedList
+
+    elif bulletType == 11:
+        for line in uselessLines():
+            if i < 10:
+                unnumberedLine = line.replace('0' + str(i + 1) + '- ', '')
+                i+=1
+
+                unnumberedList.append(unnumberedLine)
+            else:
+                unnumberedLine = line.replace(str(i+1) + '- ')
+
+                unnumberedList.append(unnumberedLine)
+
+        return unnumberedList
+
+    elif bulletType == 12:
+        for line in uselessLines():
+            if i < 10:
+                unnumberedLine = line.replace('0' + str(i + 1) + '-', '')
+                i+=1
+
+                unnumberedList.append(unnumberedLine)
+            else:
+                unnumberedLine = line.replace(str(i+1) + '-')
+
+                unnumberedList.append(unnumberedLine)
+
+        return unnumberedList
+
+    elif bulletType == 13:
+        for line in uselessLines():
+            if i < 10:
+                unnumberedLine = line.replace('0' + str(i + 1) + ' -', '')
+                i+=1
+
+                unnumberedList.append(unnumberedLine)
+            else:
+                unnumberedLine = line.replace(str(i+1) + ' -')
+
+                unnumberedList.append(unnumberedLine)
+
+        return unnumberedList
+
+def timeStampSplit():
+    songList = numberReplace()
+    songInfo = namedtuple('songInfo', ['name', 'time'])
+    REGEXS = [re.compile(r) for r in [
+            r'(?P<time>\d+:\d+)\s+(?P<name>.*)$',
+            r'(?P<name>.*)\s+(?P<time>\d+:\d+)$',
+    ]]
+
+    for item in songList:
+        for r in REGEXS:
+            m = r.match(item)
+            if m:
+                si = songInfo(**m.groupdict())
+                print si
+                break
+        else:
+            print 'oh oh' + repr(item)
+
+def timeStampToSeconds():
+
+
+downloadiT = downloadProcess()
+downloadiT.downloadLink = "https://www.youtube.com/watch?v=4dvQKe0d8DM"
+
+if __name__ == '__main__':
+    print timeStampSplit()
