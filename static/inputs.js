4d0c043ac2a98819b563b3246fb65264cb30d5d1
netsudo <netsudo@users.noreply.github.com>
2016-12-06 16:04:28 -0800

    Added placeholder text, decided not to label.

--- static/inputs.js
+++ static/inputs.js
@@ -2,17 +2,15 @@ $(document).ready(function() {
   $("#dropdown").change(function() {
       var selVal = $(this).val();
       if(selVal > 0) {
-          $("#songTitle").html('<h5>Titles:</h5>');
-          $("#timeTitle").html('<h5>Times:</h5>')
           for(var i = 1; i<= selVal; i++) {
-              $("#inputDiv").append('<br><input id="titleVal" type="text" name="titleValue" value="" />');
-              $("#inputDiv").append('<input id="timeVal" name="timeValue" value="" /><br>');
+              $("#inputDiv").append('<br><input id="titleVal" type="text" name="titleValue" value="" placeholder="Title" />');
+              $("#inputDiv").append('<input id="timeVal" name="timeValue" value="" placeholder="Time" /><br>');
           }
       }
       else {
-        $("#inputDiv").html('')
-        $("#songTitle").html('')
-        $("#timeTitle").html('')
+        $("#inputDiv").html('');
+        $("#songTitle").html('');
+        $("#timeTitle").html('');
       }
   });
 });
