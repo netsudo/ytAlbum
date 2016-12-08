d4c502d6dba7f55f90e57f83c5e917e0cfdef17c
netsudo <netsudo@users.noreply.github.com>
2016-12-07 16:35:35 -0800

    Fixed inputs, was appending oddly and formatted wrong.

--- static/inputs.js
+++ static/inputs.js
@@ -2,15 +2,14 @@ $(document).ready(function() {
   $("#dropdown").change(function() {
       var selVal = $(this).val();
       if(selVal > 0) {
+          $("#inputDiv").empty();
           for(var i = 1; i<= selVal; i++) {
               $("#inputDiv").append('<br><input id="titleVal" type="text" name="titleValue" value="" placeholder="Title" />');
               $("#inputDiv").append('<input id="timeVal" name="timeValue" value="" placeholder="Time" /><br>');
           }
       }
       else {
-        $("#inputDiv").html('');
-        $("#songTitle").html('');
-        $("#timeTitle").html('');
+        $("#inputDiv").html('')
       }
   });
 });
